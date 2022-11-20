c1381e50fca5823fc81f21e056297cf94e2e80df
Gushiken <gusiken0604@gmail.com>
2022-11-20 15:54:40 +0900

    完成

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by 具志堅 on 2022/11/20.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    
+    let rate: Double
+}
